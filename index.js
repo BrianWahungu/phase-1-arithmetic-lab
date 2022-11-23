72e97eacb91ccb0ce7ec6572e0e0f5c85111e435
BrianWahungu <cepteinhawks@gmail.com>
2022-11-23 15:04:32 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
-// Write your code here
+let num1 = 1
+let num2 = 62
+let multiply = num1 * num2 
+
+const random = Math.floor(Math.random() *35 + 16);
+
+let num3 = 4;
+let num4 = 5;
+
+let mod = num3 % num4;
+
+let max = Math.max(10, 20, 15, 8);
+console.log(max);
